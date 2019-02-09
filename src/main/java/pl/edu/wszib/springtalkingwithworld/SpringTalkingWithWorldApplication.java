8e1f848cd2a7ee24bd282e89b16bcde9ff54d2fe
Mateusz Wręczycki <setsudan.hana@gmail.com>
2019-02-09 18:52:38 +0100

    init commit

---
+++ src/main/java/pl/edu/wszib/springtalkingwithworld/SpringTalkingWithWorldApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.wszib.springtalkingwithworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTalkingWithWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTalkingWithWorldApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/edu/wszib/springtalkingwithworld/SpringTalkingWithWorldApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.edu.wszib.springtalkingwithworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTalkingWithWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
